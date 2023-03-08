e71067ecb4bf6f84ab49f3a932539f5fbefd7331
Yuto Mori <tmori9banner@gmail.com>
2023-03-08 12:16:49 +0000

    add: abc291 b

--- README.md
+++ README.md
@@ -14,5 +14,12 @@ $ oj d https://atcoder.jp/contests/abc291/tasks/abc291_a
 `CTRL + SHIFT + B`でコンパイル
 
 # 問題のチェック
-$ oj t
+$ oj t -c {コンパイルファイル}
+
+# example
+$ oj t -c ./a.out
 ```
+
+## memo
+- Dev Container 上で git を操作するために public key の設定が必要
+- 毎回設定するのは大変なので git の操作をするときだけ wsl に戻せば良い
--- contests/abc/291/b.cpp
+++ contests/abc/291/b.cpp
@@ -5,15 +5,17 @@ int main() {
     int n; // 合計人数は 5*n
     cin >> n;
 
-    vector<int> score_vec(n); // 評点の配列
-    for (int i = 0; i<n; i++){
+    vector<int> score_vec(n*5); // 評点の配列
+    for (int i = 0; i<n*5; i++){
         cin >> score_vec.at(i);
     }
 
     sort(score_vec.begin(), score_vec.end());
 
-    for (int i = 0; i<n; ++i){
-        cout << score_vec[i] << " ";
-    } 
-    cout << endl;
+    int sum_point = 0;
+    for (int i = n; i<n*4; ++i){
+        sum_point += score_vec[i];
+    }
+    double takahashi_point = (double)sum_point / (double)(3*n);
+    cout << takahashi_point << endl;
 }
